f79d6790120854690c9d97d4414ec3103c5df9c2
Marcus <marcus.peixoto@contassconsultoria.com.br>
2026-02-25 15:45:02 -0300

    dsadas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
     compatibilityDate: "2026-02-25",
     ssr: false,
     devtools: {enabled: false},
+    experimental: {
+        appManifest: false
+    },
+    vite: {
+        optimizeDeps: {
+            exclude: ["nuxt/dist/app/composables/manifest.js"]
+        }
+    },
     modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icons"],
     app: {
         head: {
